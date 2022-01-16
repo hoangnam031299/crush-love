71420129325e9e25df2f057074c9960d341b0920
hoangnam031299 <97814496+hoangnam031299@users.noreply.github.com>
2022-01-16 12:30:10 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,4 +1,4 @@
-CONFIGDATA = {
+const CONFIGDATA = {
   titleHeader: "Làm người yêu anh nhé I Love You",
   descriptionHeader:
     "Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
@@ -6,5 +6,5 @@ CONFIGDATA = {
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'Tớ biết là em sẽ đồng ý làm người yêu anh mà ^^ </br> Cám ơn em đã đồng ý để cho anh được che chở và bảo vệ em.Đối với anh, em mãi là cô bé :)) </br> "Thích em nhiều đến mấy, em sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Anh biết là em sẽ đồng ý làm người yêu anh mà ^^ </br> Cám ơn em đã đồng ý để cho anh được che chở và bảo vệ em.Đối với anh, em mãi là cô bé :)) </br> "Thích em nhiều đến mấy, em sẽ hiểu thôi. </br>  Anh thích em nhiều đến mấy, em sẽ hiểu thôi Hoàng Nam <3 Bích Phương "',
 };
